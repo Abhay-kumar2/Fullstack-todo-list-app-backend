a4b25e1cbeece8b9ab0e86998ebae79b26968c9d
Abhay-kumar2 <kr.abhay112@gmail.com>
2026-05-04 02:44:40 +0530

    new update

--- routes/ToDoRoute.js
+++ routes/ToDoRoute.js
@@ -1,25 +1,18 @@
+const { Router } = require("express");
+const {
+  getToDo,
+  saveToDo,
+  updateToDo,
+  deleteToDo,
+  toggleComplete
+} = require("../controllers/ToDoController");
 
-const {Router} = require("express");
-const { getToDo, saveToDo, updateToDo, deleteToDo, toggleComplete } = require("../controllers/ToDoController");
+const router = Router();
 
-const router =  Router()
-
-router.get('/', getToDo)
-router.post('/save', saveToDo)
-router.post('/update', updateToDo)
-router.post('/delete', deleteToDo)
+router.get('/', getToDo);
+router.post('/save', saveToDo);
+router.post('/update', updateToDo);
+router.post('/delete', deleteToDo);
 router.put('/toggle/:id', toggleComplete);
-
-
-const {Router} = require("express");
-const { getToDo, saveToDo, updateToDo, deleteToDo } = require("../controllers/ToDoController");
-
-const router =  Router()
-
-router.get('/', getToDo)
-router.post('/save', saveToDo)
-router.post('/update', updateToDo)
-router.post('/delete', deleteToDo)
-
 
 module.exports = router;
