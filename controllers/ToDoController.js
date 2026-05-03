233f92dd05f26eb67b12b6962cfddf6e1ad82a19
Abhay-kumar2 <kr.abhay112@gmail.com>
2026-05-04 02:46:46 +0530

    new update

--- controllers/ToDoController.js
+++ controllers/ToDoController.js
@@ -1,45 +1,54 @@
-
-const ToDoModel = require('../models/ToDoModel')
+const ToDoModel = require('../models/ToDoModel');
 
 module.exports.getToDo = async (req, res) => {
-    const toDo = await ToDoModel.find()
-    res.send(toDo)
-}
+  const toDo = await ToDoModel.find();
+  res.send(toDo);
+};
 
 module.exports.saveToDo = async (req, res) => {
+  const { text } = req.body;
 
-    const { text } = req.body
-
-    ToDoModel
-    .create({ text })
-    .then((data) => {
-        console.log("Added Successfully...");
-        console.log(data);
-        res.send(data)
-    })
-}
+  try {
+    const data = await ToDoModel.create({ text });
+    console.log("Added Successfully...");
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+};
 
 module.exports.updateToDo = async (req, res) => {
-    const {_id, text} = req.body
-    ToDoModel
-    .findByIdAndUpdate(_id, {text})
-    .then(()=> res.send("Updated Successfully...."))
-    .catch((err) => console.log(err))
-}
+  const { _id, text } = req.body;
+
+  ToDoModel.findByIdAndUpdate(_id, { text })
+    .then(() => res.send("Updated Successfully...."))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send(err);
+    });
+};
 
 module.exports.deleteToDo = async (req, res) => {
-    const {_id } = req.body
-    ToDoModel
-    .findByIdAndDelete(_id)
-    .then(()=> res.send("Deleted Successfully...."))
-    .catch((err) => console.log(err))
-}
+  const { _id } = req.body;
 
-const toggleComplete = async (req, res) => {
+  ToDoModel.findByIdAndDelete(_id)
+    .then(() => res.send("Deleted Successfully...."))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send(err);
+    });
+};
+
+module.exports.toggleComplete = async (req, res) => {
   try {
     const { id } = req.params;
 
     const todo = await ToDoModel.findById(id);
+
+    if (!todo) {
+      return res.status(404).send("Todo not found");
+    }
 
     todo.completed = !todo.completed;
 
@@ -47,45 +56,7 @@ const toggleComplete = async (req, res) => {
 
     res.send(todo);
   } catch (err) {
+    console.log(err);
     res.status(500).send(err);
   }
 };
-
-module.exports.toggleComplete = toggleComplete;
-
-const ToDoModel = require('../models/ToDoModel')
-
-module.exports.getToDo = async (req, res) => {
-    const toDo = await ToDoModel.find()
-    res.send(toDo)
-}
-
-module.exports.saveToDo = async (req, res) => {
-
-    const { text } = req.body
-
-    ToDoModel
-    .create({ text })
-    .then((data) => {
-        console.log("Added Successfully...");
-        console.log(data);
-        res.send(data)
-    })
-}
-
-module.exports.updateToDo = async (req, res) => {
-    const {_id, text} = req.body
-    ToDoModel
-    .findByIdAndUpdate(_id, {text})
-    .then(()=> res.send("Updated Successfully...."))
-    .catch((err) => console.log(err))
-}
-
-module.exports.deleteToDo = async (req, res) => {
-    const {_id } = req.body
-    ToDoModel
-    .findByIdAndDelete(_id)
-    .then(()=> res.send("Deleted Successfully...."))
-    .catch((err) => console.log(err))
-}
-
